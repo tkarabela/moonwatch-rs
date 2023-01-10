5a41237803149e1ff6ce39fd0c67bd979b7583ac
Tomas Karabela <tkarabela@seznam.cz>
2023-01-10 22:36:54 +0100

    Improved build scripts
    - zip/tar output
    - add version to archive name

--- build_linux.py
+++ build_linux.py
@@ -12,10 +12,17 @@ import subprocess
 import shutil
 
 TARGET_TRIPLE = "x86_64-unknown-linux-gnu"
+PKGID_URL = subprocess.check_output(["cargo", "pkgid"]).decode("utf-8").strip()
+PACKAGE_NAME_AND_VERSION = PKGID_URL.split("/")[-1].replace("#", "_")
+ARCHIVE_NAME = f"{PACKAGE_NAME_AND_VERSION}_Linux-x86-64"
+OUTPUT_ARCHIVE_PATH = op.abspath(f"build/{ARCHIVE_NAME}.tar.gz")
 
+output_build_dir = "./build"
 root_dir = op.abspath(op.dirname(__file__))
-output_dir = op.join(root_dir, f"build/{TARGET_TRIPLE}")
-os.makedirs(output_dir, exist_ok=True)
+output_dir = op.join(root_dir, f"build/{ARCHIVE_NAME}")
+if op.exists(output_dir):
+    shutil.rmtree(output_dir)
+os.makedirs(output_dir)
 
 subprocess.check_call(["cargo", "build", "--locked", "--release", "--target", f"{TARGET_TRIPLE}"])
 build_dir = op.join(root_dir, f"target/{TARGET_TRIPLE}/release")
@@ -25,3 +32,8 @@ shutil.copy(op.join(build_dir, "moonwatcher"), output_dir)
 shutil.copy(op.join(share_dir, "default-config-unix.json"), op.join(output_dir, "config.json"))
 shutil.copy(op.join(share_dir, "install_unix.py"), output_dir)
 shutil.copy(op.join(share_dir, "moonwatch-rs.service"), output_dir)
+
+if op.exists(OUTPUT_ARCHIVE_PATH):
+    os.remove(OUTPUT_ARCHIVE_PATH)
+subprocess.check_call(["tar", "cvfz", OUTPUT_ARCHIVE_PATH, ARCHIVE_NAME], cwd=output_build_dir)
+print("Created archive with build:", OUTPUT_ARCHIVE_PATH)
--- build_windows.py
+++ build_windows.py
@@ -12,10 +12,17 @@ import subprocess
 import shutil
 
 TARGET_TRIPLE = "x86_64-pc-windows-gnu"
+PKGID_URL = subprocess.check_output(["cargo", "pkgid"]).decode("utf-8").strip()
+PACKAGE_NAME_AND_VERSION = PKGID_URL.split("/")[-1].replace("#", "_")
+ARCHIVE_NAME = f"{PACKAGE_NAME_AND_VERSION}_Windows-x86-64"
+OUTPUT_ARCHIVE_PATH = op.abspath(f"build/{ARCHIVE_NAME}.zip")
 
+output_build_dir = "./build"
 root_dir = op.abspath(op.dirname(__file__))
-output_dir = op.join(root_dir, f"build/{TARGET_TRIPLE}")
-os.makedirs(output_dir, exist_ok=True)
+output_dir = op.join(root_dir, f"build/{ARCHIVE_NAME}")
+if op.exists(output_dir):
+    shutil.rmtree(output_dir)
+os.makedirs(output_dir)
 
 subprocess.check_call(["cross", "build", "--locked", "--release", "--target", f"{TARGET_TRIPLE}"])
 build_dir = op.join(root_dir, f"target/{TARGET_TRIPLE}/release")
@@ -24,3 +31,8 @@ share_dir = op.join(root_dir, "share")
 shutil.copy(op.join(build_dir, "moonwatcher.exe"), output_dir)
 shutil.copy(op.join(share_dir, "default-config-windows.json"), op.join(output_dir, "config.json"))
 shutil.copy(op.join(share_dir, "install_windows.bat"), output_dir)
+
+if op.exists(OUTPUT_ARCHIVE_PATH):
+    os.remove(OUTPUT_ARCHIVE_PATH)
+subprocess.check_call(["zip", "-r", OUTPUT_ARCHIVE_PATH, ARCHIVE_NAME], cwd=output_build_dir)
+print("Created archive with build:", OUTPUT_ARCHIVE_PATH)
