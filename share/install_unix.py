7fc828da6f7b478b136805dbacb38cdbb06b304b
Tomas Karabela <tkarabela@seznam.cz>
2024-08-10 20:10:25 +0200

    feat: Better `install_unix.py` script
    - warn about missing dependencies
    - create ~/.moonwatch-rs when not present

--- share/install_unix.py
+++ share/install_unix.py
@@ -3,15 +3,28 @@ import os.path as op
 import os
 import shutil
 import subprocess
+import sys
 
 build_dir = op.abspath(op.dirname(__file__))
 install_dir = op.expanduser("~/.moonwatch-rs")
+
+print("Testing availability of dependencies (gnome-screensaver-command, xprintidle, xdotool)")
+for cmd in (["gnome-screensaver", "-h"], ["xprintidle", "-v"], ["xdotool", "-v"]):
+    try:
+        subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+        print("Warning -", cmd, "failed, please install it before using moonwatch-rs")
+        sys.exit(1)
 
 print("Stopping moonwatch-rs service")
 rv = subprocess.call(["systemctl", "--user", "stop", "moonwatch-rs"])
 print("systemctl returned code", rv)
 
 print("Installing into", install_dir)
+if not os.path.exists(install_dir):
+    print("Creating directory", install_dir)
+    os.makedirs(install_dir)
+
 shutil.copy(op.join(build_dir, "moonwatcher"), install_dir)
 if op.exists(op.join(install_dir, "config.json")):
     print("config.json already exists, not copying default")
